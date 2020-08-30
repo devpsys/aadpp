49f292af01713df16598a7857d06919921e0b2e0
Abdurrahman Lawal Zailani <devpsys@gmail.com>
2020-08-30 23:48:04 +0100

    code refactor

--- app/src/main/java/me/devpsys/apps/gads2020/aadpp/app/ui/activities/MainActivity.kt
+++ app/src/main/java/me/devpsys/apps/gads2020/aadpp/app/ui/activities/MainActivity.kt
@@ -1,11 +1,11 @@
-package me.devpsys.apps.gads2020.aadpp.app
+package me.devpsys.apps.gads2020.aadpp.app.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import me.devpsys.apps.gads2020.aadpp.app.R
 import me.devpsys.apps.gads2020.aadpp.app.databinding.ActivityMainBinding
-import me.devpsys.apps.gads2020.aadpp.app.ui.activities.SubmitActivity
 import me.devpsys.apps.gads2020.aadpp.app.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/me/devpsys/apps/gads2020/aadpp/app/ui/activities/SplashActivity.kt
+++ app/src/main/java/me/devpsys/apps/gads2020/aadpp/app/ui/activities/SplashActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import me.devpsys.apps.gads2020.aadpp.app.MainActivity
 import me.devpsys.apps.gads2020.aadpp.app.R
 
 class SplashActivity : AppCompatActivity() {
